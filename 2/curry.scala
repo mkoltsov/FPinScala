ab3430025db868ca18069f9c6e2a91bacf2e0786
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-04-11 11:37:37 +0200

    more examples of currying added

--- 2/curry.scala
+++ 2/curry.scala
@@ -11,3 +11,18 @@ def compose[A,B,C](f: B => C, g: A => B): A => C = {
 }
 
 def partial1[A,B,C](a: A, f: (A,B) => C): B => C = (b: B) => f(a, b)
+
+def line(a:Int, b:Int,x:Int) = a*b*x
+
+def lineCurried(a:Int)(b:Int)(x:Int)=line(a,b,x)
+
+val ch = (line _).curried
+
+val p1 = ch(1)(2)
+//val p2 = p1(3)(4)
+println(p1(3))
+
+def partialLine = (x:Int, b:Int) => line(2,x,b)
+
+def partialLIne2 = line(2, _:Int, _:Int)
+
