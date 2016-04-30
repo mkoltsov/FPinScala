9372021444a92aa24ee0e28784215e1a6223805d
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-05-01 00:08:37 +0200

    chapter finished

--- 4/either.scala
+++ 4/either.scala
@@ -3,7 +3,12 @@ case class Left[+E](value: E) extends Either[E, Nothing]
 case class Right[+A](value: A) extends Either[Nothing, A]
 
 trait Either[+E, +A] {
-def map[B](f: A => B): Either[E, B]
-def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C):
-    Either[EE, C]
+def map[B](f: A => B): Either[E, B] = this match {
+	case Right(v) => Right(f(v))
+	case other =>  other
+}
+
+// def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
+// def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] 
+// def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
 }
--- 4/option.scala
+++ 4/option.scala
@@ -78,4 +78,16 @@ def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = a.foldRight
 
 def sequence2[A](a: List[Option[A]]): Option[List[A]] = traverse(a)(x=>x)
 
+def traverse[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+  es match {
+    case Nil => Right(Nil)
+    case h::t => (f(h) map2 traverse(t)(f))(_ :: _)
+  }
+
+def traverse_1[E,A,B](es: List[A])(f: A => Either[E, B]): Either[E, List[B]] = 
+  es.foldRight[Either[E,List[B]]](Right(Nil))((a, b) => f(a).map2(b)(_ :: _))
+
+def sequence[E,A](es: List[Either[E,A]]): Either[E,List[A]] = 
+  traverse(es)(x => x)
+
 
