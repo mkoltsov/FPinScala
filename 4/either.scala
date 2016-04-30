c30191f8f14ce526b3a096946f24950750dbf74d
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-04-30 09:03:17 +0200

    either completed

--- 4/either.scala
+++ 4/either.scala
@@ -7,8 +7,27 @@ def map[B](f: A => B): Either[E, B] = this match {
 	case Right(v) => Right(f(v))
 	case other =>  other
 }
+  
+def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B] =
+  this match {
+    case Left(e) => Left(e)
+    case Right(a) => f(a)
+  }
 
+def orElse[EE >: E, AA >: A](b: => Either[EE, AA]): Either[EE, AA] =
+  this match {
+    case Left(_) => b
+    case Right(a) => Right(a)
+  }
+
+def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): 
+  Either[EE, C] = for { a <- this; b1 <- b } yield f(a,b1)
 // def flatMap[EE >: E, B](f: A => Either[EE, B]): Either[EE, B]
 // def orElse[EE >: E,B >: A](b: => Either[EE, B]): Either[EE, B] 
 // def map2[EE >: E, B, C](b: Either[EE, B])(f: (A, B) => C): Either[EE, C]
 }
+
+def sequence[E, A](es: List[Either[E, A]]): Either[E, List[A]] 
+
+def traverse[E, A, B](as: List[A])(
+                      f: A => Either[E, B]): Either[E, List[B]] = as.foldRight()
