df68309594d088f4a6880bea83e7b5b184cbd71b
mkoltsov <root@javabean.ru>
2016-05-14 02:58:59 +0200

    quick recap after a loooong vacation

--- recap/2.scala
+++ recap/2.scala
@@ -11,3 +11,14 @@ def fib(n: Int): Int = {
 
 // println(fib(5))
  (3 to 10).foreach(x=> println(s"${fib(x)}"))
+
+def isSorted[A](as: Array[A])(ordered: (A,A) => Boolean): Boolean = {
+	@annotation.tailrec
+	def loop(n:Int):Boolean = 
+		if (n>=as.length-1) true
+		else if (!ordered(as(n), as(n+1))) false
+		else loop(n+1)
+	loop(0)
+}
+
+println(isSorted(Array(1,2,3,4))((x,y) => x<y))
