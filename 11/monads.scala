917a6dfd7d8ead1328af6fdcb9a024583882b016
mkoltsov <mikhail.koltsov@grandparade.co.uk>
2016-06-25 00:35:19 +0200

    monad chapter completed

--- 11/monads.scala
+++ 11/monads.scala
@@ -124,3 +124,106 @@ def filterM[A](ms: List[A])(f: A => F[Boolean]): F[List[A]] =
       if (!b) filterM(t)(f)
       else map(filterM(t)(f))(h :: _))
   }
+
+  def compose[A,B,C](f: A => F[B], g: B => F[C]): A => F[C] =
+  a => flatMap(f(a))(g)
+
+  def flatMap[A,B](ma: F[A])(f: A => F[B]): F[B] =
+  compose((_:Unit) => ma, f)(())
+
+
+  case class Id[A](value: A) {
+  def map[B](f: A => B): Id[B] = Id(f(value))
+  def flatMap[B](f: A => Id[B]): Id[B] = f(value)
+}
+
+object Id {
+  val idMonad = new Monad[Id] {
+    def unit[A](a: => A) = Id(a)
+    def flatMap[A,B](ida: Id[A])(f: A => Id[B]): Id[B] = ida flatMap f
+  }
+}
+
+case class State[S, A](run: S => (A, S)) {
+	 def map[B](f: A => B): State[S, B] =
+		State(s => {
+			val (a, s1) = run(s) (f(a), s1)
+		})
+	def flatMap[B](f: A => State[S, B]): State[S, B] =
+		State(s => {
+			val (a, s1) = run(s)
+	    	  f(a).run(s1)
+    	})
+}
+
+type IntState[A] = State[Int, A]
+
+object IntStateMonad extends Monad[IntState] {
+	def unit[A](a: => A): IntState[A] = State(s => (a, s))
+	def flatMap[A,B](st: IntState[A])(f: A => IntState[B]): IntState[B] = st flatMap f 
+}
+
+object IntStateMonad extends
+	Monad[({type IntState[A] = State[Int, A]})#IntState] { ...
+}
+
+def stateMonad[S] = new Monad[({type f[x] = State[S,x]})#f] {
+	def unit[A](a: => A): State[S,A] = State(s => (a, s))
+	def flatMap[A,B](st: State[S,A])(f: A => State[S,B]): State[S,B] = st flatMap f 
+}
+
+/ Getting and setting the same state does nothing:
+getState.flatMap(setState) == unit(())
+
+// written as for-comprehension:
+for {
+  x <- getState
+  _ <- setState(x)
+} yield ()
+
+// Setting the state to `s` and getting it back out yields `s`.
+setState(s).flatMap(_ => getState) == unit(s)
+
+// alternatively:
+for {
+  _ <- setState(s)
+  x <- getState
+} yield x
+
+
+val F = stateMonad[Int]
+def zipWithIndex[A](as: List[A]): List[(Int,A)] = as.foldLeft(F.unit(List[(Int, A)]()))((acc,a) =>
+ for {
+	xs <- acc
+            n  <- getState
+            _  <- setState(n + 1)
+	} yield (n, a) :: xs).run(0)._1.reverse
+
+object Reader {
+  def readerMonad[R] = new Monad[({type f[x] = Reader[R,x]})#f] {
+    def unit[A](a: => A): Reader[R,A] = Reader(_ => a)
+    def flatMap[A,B](st: Reader[R,A])(f: A => Reader[R,B]): Reader[R,B] =
+      Reader(r => f(st.run(r)).run(r))
+  }
+
+  // A primitive operation for it would be simply to ask for the `R` argument:
+  def ask[R]: Reader[R, R] = Reader(r => r)
+}
+
+// The action of Reader's `flatMap` is to pass the `r` argument along to both the
+// outer Reader and also to the result of `f`, the inner Reader. Similar to how
+// `State` passes along a state, except that in `Reader` the "state" is read-only.
+
+// The meaning of `sequence` here is that if you have a list of functions, you can
+// turn it into a function that takes one argument and passes it to all the functions
+// in the list, returning a list of the results.
+
+// The meaning of `join` is simply to pass the same value as both arguments to a
+// binary function.
+
+// The meaning of `replicateM` is to apply the same function a number of times to
+// the same argument, returning a list of the results. Note that if this function
+// is _pure_, (which it should be), this can be exploited by only applying the
+// function once and replicating the result instead of calling the function many times.
+// This means the Reader monad can override replicateM to provide a very efficient
+// implementation.
